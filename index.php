088c94a89032dbe6ae871a610b39052623e1c9a2
Muneef Hameed <hey@muneef.in>
2011-11-16 22:24:31 +0530

    one step ahead of OOP

--- index.php
+++ index.php
@@ -1,29 +1,80 @@
+<div style="background-color:blue; padding:30px; color:yellow">
 <pre>
 <?php 
 
-$district='wayanad';
-if(isset($_GET['q'])){
-	$district=$_GET['q'];
-}else{
+/** 
+	table titles from IMD
+ **/
+
+function names($v=false){
+	$names=array('rainfall'=>'Rainfall (mm)',
+			'max-temp'=>'Max Temperature ( deg C)',
+			'min-temp'=>'Min Temperature ( deg C)',
+			'total-cloud-cover'=>'Total cloud cover (octa)',
+			'max-rel-hum'=>'Max Relative Humidity (%)',
+			'mind-rel-hum'=>'Min Relative Humidity (%)',
+			'wind-speed'=>'Wind speed (kmph)',
+			'wind-dir'=>'Wind direction (deg)',
+			);
+	if($v){
+	$i=0;
+	foreach ($names as $key=>$value){
+		$yoyo[$key]=array(
+						'id'=>$i,
+						'value'=>$key,
+						'title'=>$value,
+					);
+		$i++;
+	}
+	
+	return $yoyo;
+	}else{
+		return $names;
+	}
+
+}
+	$count=0;
+
+function get_name(){
+	/** default city **/
 	$district='kozhikode';
+	if(isset($_GET['q'])){
+		$district=$_GET['q'];
+	}
+	$dist=$district;
+	return $dist;
 }
 
-$source='http://www.imd.gov.in/section/nhac/distforecast/'.$district.'.htm';
-$var=file_get_contents($source);
+function get_city_data($dist){
+	if(!$dist)
+		return ;
+	$source=source($dist);
+	$var=file_get_contents($source);
 
-// parse one
-preg_match_all("/\s+(\d)+\s+(\d)+\s+(\d)+\s+(\d)+\s+(\d)+/",$var, $phones);
-foreach($phones[0] as $one){
+	// parse one
+	preg_match_all("/\s+(\d)+\s+(\d)+\s+(\d)+\s+(\d)+\s+(\d)+/",$var, $phones);
+	foreach($phones[0] as $one){
 		$ones[]=separate($one);
-};
+	};
+	
+	return $ones;
+		
+}
+
+
+/** silly migration stuffs, you know **/
+
+
+function source($dist){
+	return 'http://www.imd.gov.in/section/nhac/distforecast/'.$dist.'.htm';
+}
+$source=source($dist);
+/** ends **/
 
 function separate($string){
 	preg_match_all("/\s{3}+\d+/",$string, $ones);
 	return $ones;
 }
-
-$names=array('Rainfall (mm)','Max Temperature ( deg C)','Min Temperature ( deg C)','Total cloud cover (octa)','Max Relative Humidity (%)','Min Relative Humidity (%)','Wind speed (kmph)','Wind direction (deg)');
-$count=0;
 
 function showall($ones){
 	foreach($ones as $o){
@@ -36,14 +87,39 @@ function showall($ones){
 	}
 }
 
-
-//print 'Rainfall';
-show_particular($ones,1,$names[1],1);
-show_particular($ones,2,$names[2],2);
-
-//print 'Rainfall';
-
-function show_particular($ones,$index,$names,$va=1){
-	print '\''.$names.'\' : {'.$ones[$va][0][0].'}';
+function look_up($day=1,$results=array()){
+	
 }
+
+function show_particular($name=null,$index='max-temp',$day=0){
+
+	$name=get_name();	
+	$ones=get_city_data($name);
+	// get data titles
+	
+	if(is_array($index)){
+		foreach($index as $m){
+			print loo($m,$ones,$day);
+		}
+
+	}
+	
+}
+
+function loo($index,$ones,$day=1){
+	$names=names(true);
+	// title, id
+	$title=$names[$index]['title'];	
+	$id=$names[$index]['id'];
+	$to= $ones;
+	$value=$to[$id][0][$day];
+	return $title.' : '.$value.'<br/>';
+}
+
+
+//print 'Rainfall';
+show_particular(null,array('max-temp','min-temp','wind-dir'),2);
+
+
 ?>
+</div>
